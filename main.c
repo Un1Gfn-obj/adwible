d3c361280d32bc942192c09ffba6f8b531c7892a
Darren Ng <Un1Gfn@gmail.com>
2022-08-23 15:20:12 +0800

    adwdemo list stub
    Signed-off-by: Darren Ng <Un1Gfn@gmail.com>

--- main.c
+++ main.c
@@ -6,7 +6,6 @@
 
 static inline GResource *g_resources_register2(){
 
-  
   GResource *r=j72qkd_get_resource(); _Static_assert(NULL==G_RESOURCE_ERROR_NOT_FOUND, ""); g_assert_true(r);
   // const char *path="/com/un1gfn/ck3fm7";
   // g_print("%s\n", path);
@@ -52,8 +51,15 @@ static void activate_cb(AdwApplication *app){
   // https://gitlab.gnome.org/GNOME/libadwaita/-/blob/1.1.4/demo/adw-demo-window.c#L118
   // g_type_ensure(...);
   // adw_init();
-  GtkBuilder *b=gtk_builder_new_from_resource("/com/un1gfn/ck3fm7/b7cj8w.ui"); g_assert_true(b);
+  // GtkBuilder *b=gtk_builder_new_from_resource("/com/un1gfn/ck3fm7/b7cj8w.ui"); g_assert_true(b);
+  GtkBuilder *b=gtk_builder_new_from_resource("/com/un1gfn/ck3fm7/w59xv6.ui"); g_assert_true(b);
   GObject *const w=gtk_builder_get_object(b, "tf2fhx"); g_assert_true(w);
+
+  // GtkFlowBox no animation
+  // GObject *const fb=gtk_builder_get_object(b, "dx7fws"); g_assert_true(fb);
+  // gulong signalID=g_signal_lookup("child-activated", gtk_flow_box_get_type());
+  // g_assert_true(1<signalID && signalID<(gulong)(-2));
+  // g_assert_true(0==g_signal_handlers_disconnect_matched(fb, G_SIGNAL_MATCH_ID, signalID, 0, NULL, NULL, NULL));
 
   if(!isMobile()){
     g_message("not wt88047, resizing...");
@@ -63,8 +69,8 @@ static void activate_cb(AdwApplication *app){
   // render window
   gtk_application_add_window(GTK_APPLICATION(app), GTK_WINDOW(w));
   g_object_unref(G_OBJECT(b)); b=NULL;
-  gtk_window_present(GTK_WINDOW(w));
-  // gtk_widget_show(GTK_WIDGET(w));
+  // gtk_window_present(GTK_WINDOW(w));
+  gtk_widget_show(GTK_WIDGET(w));
 
 } 
 
