7133d4db151993be24055251e9d650c131d85b95
Darren Ng <Un1Gfn@gmail.com>
2022-08-23 16:50:02 +0800

    todo: OT/NT (swipe pane) Carousel
    Signed-off-by: Darren Ng <Un1Gfn@gmail.com>

--- main.c
+++ main.c
@@ -46,31 +46,33 @@ static inline gboolean isMobile(){
 
 static void activate_cb(AdwApplication *app){
 
-  // get window
   // segfault if not initialized
   // https://gitlab.gnome.org/GNOME/libadwaita/-/blob/1.1.4/demo/adw-demo-window.c#L118
   // g_type_ensure(...);
   // adw_init();
-  GtkBuilder *b=gtk_builder_new_from_resource("/com/un1gfn/ck3fm7/b7cj8w.ui"); g_assert_true(b);
-  // GtkBuilder *b=gtk_builder_new_from_resource("/com/un1gfn/ck3fm7/w59xv6.ui"); g_assert_true(b);
-  GObject *const w=gtk_builder_get_object(b, "tf2fhx"); g_assert_true(w);
+  GtkBuilder *x[]={
+    gtk_builder_new_from_resource("/com/un1gfn/ck3fm7/w59xv6.ui"), // adwdemo.list
+    gtk_builder_new_from_resource("/com/un1gfn/ck3fm7/b7cj8w.ui"), // adwible
+  };
+  _Static_assert(sizeof(x)==2*sizeof(void*));
+  for(gsize i=0; i<sizeof(x)/sizeof(void*); ++i){
+    GtkBuilder *b=x[i];
+    g_assert_true(b);
+    GObject *const w=gtk_builder_get_object(b, "tf2fhx"); g_assert_true(w);
+    if(!isMobile()){
+      g_message("not wt88047, resizing...");
+      gtk_window_set_default_size(GTK_WINDOW(w), 720/2, 1000/2);
+    }
+    gtk_application_add_window(GTK_APPLICATION(app), GTK_WINDOW(w));
+    g_object_unref(G_OBJECT(b)); b=NULL;
+    gtk_widget_show(GTK_WIDGET(w)); // gtk_window_present(GTK_WINDOW(w));
+  }
 
   // GtkFlowBox no animation
   // GObject *const fb=gtk_builder_get_object(b, "dx7fws"); g_assert_true(fb);
   // gulong signalID=g_signal_lookup("child-activated", gtk_flow_box_get_type());
   // g_assert_true(1<signalID && signalID<(gulong)(-2));
   // g_assert_true(0==g_signal_handlers_disconnect_matched(fb, G_SIGNAL_MATCH_ID, signalID, 0, NULL, NULL, NULL));
-
-  if(!isMobile()){
-    g_message("not wt88047, resizing...");
-    gtk_window_set_default_size(GTK_WINDOW(w), 720/2, 1000/2);
-  }
-
-  // render window
-  gtk_application_add_window(GTK_APPLICATION(app), GTK_WINDOW(w));
-  g_object_unref(G_OBJECT(b)); b=NULL;
-  // gtk_window_present(GTK_WINDOW(w));
-  gtk_widget_show(GTK_WIDGET(w));
 
 }
 
